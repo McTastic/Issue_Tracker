f321223509fb9989e2edc46248020a7a7a5b7923
Sean M <mctastic2121@gmail.com>
2023-07-11 16:46:21 -0400

    all CRUD routes working for user now

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -5,12 +5,16 @@ const prisma = new PrismaClient();
 
 // get a single user
 export async function GET(request: NextRequest) {
-    const userId = request.url.split("/")[5];
+    // got the id from the URL because I could not figure out how to get it from the request in any other way
+  const userId = request.url.split("/")[5];
   try {
-    const id  = userId;
+    const id = userId;
 
     if (!id) {
-      return NextResponse.json({ status: 400, error: "Missing 'id' parameter" });
+      return NextResponse.json({
+        status: 400,
+        error: "Missing 'id' parameter",
+      });
     }
 
     const user = await prisma.user.findUnique({
@@ -20,12 +24,78 @@ export async function GET(request: NextRequest) {
     });
 
     if (!user) {
-      return NextResponse.json({ status:404, error: "User not found" });
+      return NextResponse.json({ status: 404, error: "User not found" });
     }
 
     return NextResponse.json(user);
   } catch (error) {
     console.error(error);
-    return NextResponse.json({  status: 500, error: "An error occurred" });
+    return NextResponse.json({ status: 500, error: "An error occurred" });
   }
 }
+
+// update a single user
+export async function PUT(request: NextRequest) {
+    const userId = request.url.split("/")[5];
+    try {
+        const id = userId;
+        const { name, email, password } = await request.json();
+
+        if (!id) {
+        return NextResponse.json({
+            status: 400,
+            error: "Missing 'id' parameter",
+        });
+        }
+
+        const user = await prisma.user.update({
+        where: {
+            id: String(id),
+        },
+        data: {
+            name,
+            email,
+            password,
+        },
+        });
+
+        if (!user) {
+        return NextResponse.json({ status: 404, error: "User not found" });
+        }
+
+        return NextResponse.json(user);
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({ status: 500, error: "An error occurred" });
+    }
+    }
+
+// delete a single user
+export async function DELETE(request: NextRequest) {
+    const userId = request.url.split("/")[5];
+    try {
+        const id = userId;
+    
+        if (!id) {
+        return NextResponse.json({
+            status: 400,
+            error: "Missing 'id' parameter",
+        });
+        }
+    
+        const user = await prisma.user.delete({
+        where: {
+            id: String(id),
+        },
+        });
+    
+        if (!user) {
+        return NextResponse.json({ status: 404, error: "User not found" });
+        }
+    
+        return NextResponse.json(user);
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({ status: 500, error: "An error occurred" });
+    }
+    }
--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,4 +1,3 @@
-import { NextApiRequest, NextApiResponse } from "next";
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcrypt";
 
@@ -37,30 +36,3 @@ export async function POST(request: Request) {
       return new Response("An error occurred", { status: 500 });
     }
   }
-
-// export default async function handler(
-//     req:NextApiRequest, 
-//     res:NextApiResponse
-//     ) {
-//         // const prisma = new PrismaClient();
-//     if(req.method ==="GET"){
-//         res.status(200).json({message: "GET request"});
-//     } else if (req.method === "POST"){
-//         const {name, email,password} = req.body;
-//         const user = await prisma.user.create({
-//             data: {
-//                 name: name,
-//                 email: email,
-//                 password: password,
-//             }
-//         });
-//         res.status(200).json(user);
-//     } 
-//     else if (req.method === "PUT"){
-//         res.status(200).json({message: "PUT request"});
-//     }
-//     else if (req.method === "DELETE"){
-//         res.status(200).json({message: "DELETE request"});
-//     }
-//     res.status(405).json({message: "Method not allowed"});
-// }
