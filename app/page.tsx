a3d1440dfd742374222d20e0bede48c3a9ae2b1f
Sean M <mctastic2121@gmail.com>
2023-07-16 20:26:16 -0400

    removing non necessary css properties

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,6 @@ export default function Home() {
             Dashboard
           </Link>
           <Login />
-          <Link className={styles.signUp} href="/dashboard" prefetch={false}>Don't have an account?</Link>
         </div>
       </div>
     </div>
