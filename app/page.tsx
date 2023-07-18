94134f651ba0df70d530a959d3523d51367b8fb1
Sean M <mctastic2121@gmail.com>
2023-07-18 19:15:39 -0400

    added new colors and an image to homepage/login

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
 import styles from "../public/css/homepage.module.css";
 import Link from "next/link";
+import LaptopImage from "../public/images/laptop-image.png";
 import Login from "./components/login";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className={styles.flex}>
       <div className={styles.leftSide}>
-        <h1 style={{ fontSize: "55px" }}>This is home</h1>
+        {/* <h1 style={{ fontSize: "55px" }}>This is home</h1> */}
+        <Image className={styles.homeImage}src={LaptopImage} alt="laptop" />
       </div>
       <div className={styles.rightSide}>
         <div className={styles.container}>
